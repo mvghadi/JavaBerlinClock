24ddd0fa9b54c6aea33dd3b5c5ff7c7fd8f205b4
mvghadi <38432693+mvghadi@users.noreply.github.com>
2018-04-18 19:41:26 +0530

    Create Testing.java

---
+++ src/main/java/com/ubs/opsit/interviews/test/Testing.java
@@ -0,0 +1,9 @@
+package com.ubs.opsit.interviews.test;
+
+import com.ubs.opsit.interviews.BerlinTimeConverter;
+
+public class Testing {
+	public static void main(String[] args) {
+		System.out.println(new BerlinTimeConverter().convertTime("24:00:01"));
+	}
+}
